40015c532310990aec9cb29e065bdeb34931afa0
Angelo Machado Fonseca <angelofmfonseca@gmail.com>
2019-07-23 23:19:00 -0300

    validação de inputs

---
+++ index.js
@@ -0,0 +1,62 @@
+let submitButton = document.querySelector("#submitButton");
+
+submitButton.addEventListener("click", function(event){
+  event.preventDefault();
+
+  let form = document.querySelector("#champions");
+  
+  let champions = getValues(form);
+
+  let newTr = createTr(champions);
+
+  isValid(champions);
+
+  let tableBody = document.querySelector("tbody");
+  tableBody.appendChild(newTr);
+
+  form.reset();
+})
+
+function getValues(form){
+  let champions = {
+    time: form.time.value,
+    jogos: form.jogos.value,
+    gols: form.gols.value,
+    artilheiro: form.artilheiro.value
+  }
+  return champions;
+}
+
+function createTr(champions){
+  let newTr = document.createElement("tr");
+
+  let timeTd = createTd(champions.time);
+  let jogosTd = createTd(champions.jogos);
+  let golsTd = createTd(champions.gols);
+  let artilheiroTd = createTd(champions.artilheiro);
+
+  newTr.appendChild(timeTd);
+  newTr.appendChild(jogosTd);
+  newTr.appendChild(golsTd);
+  newTr.appendChild(artilheiroTd);
+
+  return newTr;
+}
+
+function createTd(dado){
+  let newTd = document.createElement("td");
+  newTd.textContent = dado;
+
+  return newTd;
+}
+
+function isValid(champions){
+  if(champions.jogos < 0){
+    let jogosErro = document.querySelector("#error-message");
+    jogosErro.textContent = "Número inválido de jogos";
+  }
+  if(champions.gols < 0){
+    let golsErro = document.querySelector("#error-message");
+    golsErro.textContent = "Número inválido de gols";
+  }
+}
